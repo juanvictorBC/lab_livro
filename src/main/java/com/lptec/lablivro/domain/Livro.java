844225eecb9aa0d2611d23419b5d25e3d89e3028
Juan Victor <juanvictor_bc@hotmail.com>
2024-11-18 18:00:37 -0300

    inserção da anotação @id na entidade

--- src/main/java/com/lptec/lablivro/domain/Livro.java
+++ src/main/java/com/lptec/lablivro/domain/Livro.java
@@ -3,11 +3,17 @@ package com.lptec.lablivro.domain;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Livro {
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String titulo;
 	private String autor;
 	private String editora;
