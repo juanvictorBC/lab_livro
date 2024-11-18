5bae932ee4a62875e6a4680526b99bed106cfd0e
Juan Victor <juanvictor_bc@hotmail.com>
2024-11-18 15:52:51 -0300

    Criacao do projeto com suas dependencias

---
+++ src/main/java/com/lptec/lablivro/LablivroApplication.java
@@ -0,0 +1,13 @@
+package com.lptec.lablivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LablivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LablivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lptec/lablivro/LablivroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lptec.lablivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LablivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
